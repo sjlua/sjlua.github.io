92c34d3f7aa2ee32318a546f73f0176ef17cc15d
sirsleon <seanjlua10@gmail.com>
2024-07-10 00:02:54 +1000

    Web Version 0.3

--- spine.js
+++ spine.js
@@ -1,1 +1,11 @@
 // The 'spine' of the webpage, the backend, per se.
+
+// The following manages the hamburger menu.
+function toggleMenu() {
+    const menu = document.querySelector(".hamburgerLinks");
+    const icon = document.querySelector(".hamburgerIcon");
+    
+    // This toggles the class="open" which is attributed to both hamburgerLinks and hamburgerIcon.
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
